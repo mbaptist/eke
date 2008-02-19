aba2c4024950022ec239a97ac3a0d47b517d9346
mbaptist <mbaptist@d833ded2-d844-0410-b6fc-eb25fe045107>
2008-02-19 19:54:04 +0000

    19022088

--- eke.hpp
+++ eke.hpp
@@ -7,34 +7,24 @@
 
 using namespace blitz;
 
+#include "grid.hpp"
 
-void initialise_grid(Array<TinyVector<double,3>,3> & coordinates,
-		     Array<int,3> & point_type,
-		     const int & nx, const int & ny,const int & nz,
-		     const double & lx, const double & ly,const double & lz,
-		     const double & rs);
-
-void initialise_colloid(Array<double,3> & charge_density,
-			Array<int,3> & point_type,
-			const Array<TinyVector<double,3>,3> & coordinates,
-			const int & nx, const int & ny,const int & nz,
-			const double & lx, const double & ly,const double & lz,
-			const double & rs, const double & charge);
+void initialise_colloid(Array<double,3> & charge_density, const Grid & grid, const double & charge);
 
 
 void initialise_electric_field(Array<TinyVector<double,3>,3> & electric_field,
-              const Array<double,3> & charge_density,
-			       const Array<int,3> & point_type,
-			       const int & nx, const int & ny,const int & nz,
-			       const double & lx, const double & ly,const double & lz);  
+                              const Array<double,3> & charge_density,
+			      const Array<int,3> & point_type,
+			      const int & nx, const int & ny,const int & nz,
+			      const double & lx, const double & ly,const double & lz);  
 
 
 double random_loop_move(Array<TinyVector<double,3>,3> & electric_field,
-		 const int & nx,const int & ny,const int & nz,
-		 const double & lx, const double & ly,const double & lz);
+		const int & nx,const int & ny,const int & nz,
+		const double & lx, const double & ly,const double & lz);
 
 double sequential_loop_move(Array<TinyVector<double,3>,3> & electric_field,
-		 const int & nx,const int & ny,const int & nz,
+		const int & nx,const int & ny,const int & nz,
 			    const double & lx,const double & ly,const double & lz);
 
 
--- geometry.hpp
+++ geometry.hpp
@@ -6,6 +6,17 @@ class GeometryObject
 };
 
 
+class Sphere: public GeometryObject
+{
+  
+};
+
+
+class Cube: public GeometryObject
+{
+};
+
+
 class Geometry: public blitz::Array<GeometryObject,1>
 {
 };
