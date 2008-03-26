3cba641a3dee838d15a6467658b3693fe34d7d17
mbaptist <mbaptist@d833ded2-d844-0410-b6fc-eb25fe045107>
2008-03-26 14:35:03 +0000

    License moved to GPL v3

--- trunk/grid.hpp
+++ trunk/grid.hpp
@@ -1,3 +1,35 @@
+// -*- C++ -*-
+
+//
+// C++ Interface: grid
+//
+// Description: 
+//
+//
+// Author: Manuel Baptista <mbaptist@gmail.com>, (C) 2008
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+
+/*
+Copyright (C) 2008 Manuel Baptista <mbaptist@gmail.com>
+
+This file is part of eke.
+
+Eke is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Eke is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with eke.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef GRID_HPP
 #define GRID_HPP
